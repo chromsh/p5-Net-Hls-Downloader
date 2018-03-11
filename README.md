6b050e35f24f948d070d3409476f7120a0a90be6
chrom <chrom@chromsh.com>
2018-03-11 12:50:46 +0900

    initial import

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Net::Hls::Downloader - It's new $module
+
+# SYNOPSIS
+
+    use Net::Hls::Downloader;
+
+# DESCRIPTION
+
+Net::Hls::Downloader is ...
+
+# LICENSE
+
+Copyright (C) chrom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+chrom <chromsh@gmail.com>
---
+++ lib/Net/Hls/Downloader.pm
@@ -0,0 +1,39 @@
+package Net::Hls::Downloader;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Hls::Downloader - It's new $module
+
+=head1 SYNOPSIS
+
+    use Net::Hls::Downloader;
+
+=head1 DESCRIPTION
+
+Net::Hls::Downloader is ...
+
+=head1 LICENSE
+
+Copyright (C) chrom.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+chrom E<lt>chromsh@gmail.comE<gt>
+
+=cut
+
